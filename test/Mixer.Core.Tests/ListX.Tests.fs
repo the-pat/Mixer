1f90b3440337e24a5c1820a083c81347007c2f85
Patrick Tone <patrick@madronesoftware.com>
2021-03-04 16:38:18 +0100

    Adds additional test to cover the problem statement

--- test/Mixer.Core.Tests/ListX.Tests.fs
+++ test/Mixer.Core.Tests/ListX.Tests.fs
@@ -1,5 +1,6 @@
 module ListX.Tests
 
+open System.Collections.Generic
 open Xunit
 open Mixer.Core
 
@@ -7,7 +8,13 @@ open Mixer.Core
 let ``ListX.shuffled 1 10_0000 returns a shuffled list with values between 1 and 10,000 inclusive`` () =
     let expectedValues = Set.ofSeq { 1 .. 10_000 }
 
-    let actual = ListX.shuffledList 1 10_000
+    let actual : int list = ListX.shuffledList 1 10_000
 
-    Assert.IsType<int list>(actual)
     Assert.All(actual, (fun elem -> Assert.True(Set.contains elem expectedValues)))
+
+[<Fact>]
+let ``ListX.shuffled 1 10_0000 run twice does not return the same result`` () =
+    let shuffled1 = ListX.shuffledList 1 10_000
+    let shuffled2 = ListX.shuffledList 1 10_000
+
+    Assert.NotEqual<IEnumerable<int>>(shuffled1, shuffled2)
